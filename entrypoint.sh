2563d47875c26dfc602228f436f89f00cd3cc17d
gregnuj <greg@terra.money>
2023-08-24 09:29:44 -0500

    verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -u
 
 REPO_NAME="${INPUT_REPOSITORY_NAME}"
 AWS_REGION="${INPUT_AWS_REGION}"
+CC_URL="https://git-codecommit.${AWS_REGION}.amazonaws.com/v1/repos/${REPO_NAME}"
+
+set -x
 
 # Check if the repository exists
 aws codecommit get-repository --repository-name $REPO_NAME > /dev/null 2>&1
@@ -21,7 +24,6 @@ fi
 
 set -e
 
-CC_URL="https://git-codecommit.${AWS_REGION}.amazonaws.com/v1/repos/${REPO_NAME}"
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 git config --global --add safe.directory /github/workspace
