7a764b1310e9998540977a196b838a6011c67ed1
gregnuj <greg@terra.money>
2023-08-24 08:37:05 -0500

    create repo if it does not exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
 
-set -ue
+set -u
 
-RepositoryName="${INPUT_REPOSITORY_NAME}"
-AwsRegion="${INPUT_AWS_REGION}"
-CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
+REPO_NAME="${INPUT_REPOSITORY_NAME}"
+AWS_REGION="${INPUT_AWS_REGION}"
 
+# Check if the repository exists
+aws codecommit get-repository --repository-name $REPO_NAME > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Repository $REPO_NAME already exists."
+else
+  echo "Repository $REPO_NAME does not exist. Creating now..."
+  aws codecommit create-repository --repository-name $REPO_NAME
+  if [ $? -eq 0 ]; then
+    echo "Repository $REPO_NAME created successfully."
+  else
+    echo "Failed to create repository $REPO_NAME."
+  fi
+fi
+
+set -e
+
+CC_URL="https://git-codecommit.${AWS_REGION}.amazonaws.com/v1/repos/${REPO_NAME}"
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 git config --global --add safe.directory /github/workspace
-git remote add sync ${CodeCommitUrl}
+git remote add sync ${CC_URL}
 git push sync --mirror
