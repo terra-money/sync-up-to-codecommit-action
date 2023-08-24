ff33175bde9a239c4f0f500cccf42fdadb933f75
gregnuj <greg@terra.money>
2023-08-24 07:28:17 -0500

    add safe dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,6 @@ CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${Repo
 
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
+git config --global --add safe.directory /github/workspace
 git remote add sync ${CodeCommitUrl}
 git push sync --mirror
